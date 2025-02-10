f73fadefbd30865d0903ab219dcf092aa68f5d0b
Deneb Donoso Duran <145538409+Xicobot@users.noreply.github.com>
2025-02-10 13:44:06 +0100

    Update installandconfig.sh

--- installandconfig.sh
+++ installandconfig.sh
@@ -29,11 +29,11 @@ read -p "⚠️ ¿Quieres agregar esta IP a Suricata? (s/n): " confirm
 if [ "$confirm" = "s" ] || [ "$confirm" = "S" ]; then
 
 	sudo tee -a /var/lib/suricata/rules/local.rules > /dev/null <<EOL
-alert tcp $IP any -> any any (msg:"Nmap SYN Scan Detected"; flags:S; threshold:type both, track by_src, count 10, seconds 10; sid:1000001; rev:1;)
-alert udp $IP any -> any any (msg:"Nmap UDP Scan Detected"; threshold:type both, track by_src, count 10, seconds 10; sid:1000002; rev:1;)
-alert tcp $IP any -> any any (msg:"Nmap NULL Scan Detected"; flags:0; threshold:type both, track by_src, count 5, seconds 10; sid:1000003; rev:1;)
-alert tcp $IP any -> any any (msg:"Nmap FIN Scan Detected"; flags:F; threshold:type both, track by_src, count 5, seconds 10; sid:1000004; rev:1;)
-alert tcp $IP any -> any any (msg:"Nmap Xmas Scan Detected"; flags:FPU; threshold:type both, track by_src, count 5, seconds 10; sid:1000005; rev:1;)
+alert tcp $IP any -> any any (msg:"Nmap SYN Scan Detected on $choice"; flags:S; threshold:type both, track by_src, count 10, seconds 10; sid:1000001; rev:1;)
+alert udp $IP any -> any any (msg:"Nmap UDP Scan Detected on $choice"; threshold:type both, track by_src, count 10, seconds 10; sid:1000002; rev:1;)
+alert tcp $IP any -> any any (msg:"Nmap NULL Scan Detected on $choice"; flags:0; threshold:type both, track by_src, count 5, seconds 10; sid:1000003; rev:1;)
+alert tcp $IP any -> any any (msg:"Nmap FIN Scan Detected on $choice"; flags:F; threshold:type both, track by_src, count 5, seconds 10; sid:1000004; rev:1;)
+alert tcp $IP any -> any any (msg:"Nmap Xmas Scan Detected on $choice"; flags:FPU; threshold:type both, track by_src, count 5, seconds 10; sid:1000005; rev:1;)
 EOL
 
     echo "🔄 Recargando Suricata..."
