bccc4d87bb17f126ad847b7fd6ae8a5d187b77f7
Deneb Donoso Duran <145538409+Xicobot@users.noreply.github.com>
2025-02-10 13:43:00 +0100

    Update installandconfig.sh

--- installandconfig.sh
+++ installandconfig.sh
@@ -28,7 +28,7 @@ read -p "⚠️ ¿Quieres agregar esta IP a Suricata? (s/n): " confirm
 
 if [ "$confirm" = "s" ] || [ "$confirm" = "S" ]; then
 
-	sudo tee -a /etc/suricata/rules/local.rules > /dev/null <<EOL
+	sudo tee -a /var/lib/suricata/rules/local.rules > /dev/null <<EOL
 alert tcp $IP any -> any any (msg:"Nmap SYN Scan Detected"; flags:S; threshold:type both, track by_src, count 10, seconds 10; sid:1000001; rev:1;)
 alert udp $IP any -> any any (msg:"Nmap UDP Scan Detected"; threshold:type both, track by_src, count 10, seconds 10; sid:1000002; rev:1;)
 alert tcp $IP any -> any any (msg:"Nmap NULL Scan Detected"; flags:0; threshold:type both, track by_src, count 5, seconds 10; sid:1000003; rev:1;)
