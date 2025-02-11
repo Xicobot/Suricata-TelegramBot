a2c3f11e85701bcc5cac32d8d6b7dfc7bd92faa8
Deneb Donoso Duran <145538409+Xicobot@users.noreply.github.com>
2025-02-11 13:59:07 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-apt update
-apt upgrade
-apt install suricata
-
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y suricata
 
 echo "Detectando interfaces de red disponibles..."
 ip -o link show | awk -F': ' '{print NR". "$2}'
@@ -21,7 +20,6 @@ fi
 echo "Has seleccionado la interfaz: $INTERFACE"
 
 echo "Añadiendo la interfaz al archivo /etc/suricata/suricata.yaml"
-
 sudo sed -i "s/interface: .*/interface: $INTERFACE/" /etc/suricata/suricata.yaml
 
 IP=$(ip -4 addr show "$INTERFACE" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
@@ -37,19 +35,36 @@ read -p "¿Quieres agregar esta IP a Suricata? (s/n): " confirm
 
 if [ "$confirm" = "s" ] || [ "$confirm" = "S" ]; then
 
-	sudo tee -a /var/lib/suricata/rules/local.rules > /dev/null <<EOL
-alert tcp $IP any -> any any (msg:"Nmap SYN Scan Detected on $choice"; flags:S; threshold:type both, track by_src, count 10, seconds 10; sid:1000001; rev:1;)
-alert udp $IP any -> any any (msg:"Nmap UDP Scan Detected on $choice"; threshold:type both, track by_src, count 10, seconds 10; sid:1000002; rev:1;)
-alert tcp $IP any -> any any (msg:"Nmap NULL Scan Detected on $choice"; flags:0; threshold:type both, track by_src, count 5, seconds 10; sid:1000003; rev:1;)
-alert tcp $IP any -> any any (msg:"Nmap FIN Scan Detected on $choice"; flags:F; threshold:type both, track by_src, count 5, seconds 10; sid:1000004; rev:1;)
-alert tcp $IP any -> any any (msg:"Nmap Xmas Scan Detected on $choice"; flags:FPU; threshold:type both, track by_src, count 5, seconds 10; sid:1000005; rev:1;)
+    echo "¿Qué reglas quieres usar?"
+    echo "1: Reglas personalizadas (Recomendado)"
+    echo "2: Reiniciar Suricata sin agregar reglas"
+    read -p "Elige una opción (1 o 2): " OP1
+
+    if [ -z "$OP1" ]; then
+        echo "Opción inválida. Saliendo..."
+        exit 1
+    fi
+
+    if [ "$OP1" = "1" ]; then
+        sudo tee -a /var/lib/suricata/rules/local.rules > /dev/null <<EOL
+alert tcp $IP any -> any any (msg:"Nmap SYN Scan Detected on $INTERFACE"; flags:S; threshold:type both, track by_src, count 10, seconds 10; sid:1000001; rev:1;)
+alert udp $IP any -> any any (msg:"Nmap UDP Scan Detected on $INTERFACE"; threshold:type both, track by_src, count 10, seconds 10; sid:1000002; rev:1;)
+alert tcp $IP any -> any any (msg:"Nmap NULL Scan Detected on $INTERFACE"; flags:0; threshold:type both, track by_src, count 5, seconds 10; sid:1000003; rev:1;)
+alert tcp $IP any -> any any (msg:"Nmap FIN Scan Detected on $INTERFACE"; flags:F; threshold:type both, track by_src, count 5, seconds 10; sid:1000004; rev:1;)
+alert tcp $IP any -> any any (msg:"Nmap Xmas Scan Detected on $INTERFACE"; flags:FPU; threshold:type both, track by_src, count 5, seconds 10; sid:1000005; rev:1;)
 EOL
 
-    echo "Recargando Suricata..."
-   sed -i 's/^\([[:space:]]*-\s*\)suricata\.rules/\1local.rules/'
-    sudo systemctl restart suricata
+        sudo sed -i 's/^\([[:space:]]*-\s*\)suricata\.rules/\1local.rules/' /etc/suricata/suricata.yaml
 
-    echo "¡Regla agregada y Suricata reiniciado!"
-else
-    echo "No se hicieron cambios. Saliendo..."
+    elif [ "$OP1" = "2" ]; then
+        echo "Recargando Suricata..."
+	sudo suricata-update list-sources
+	sudo suricata-update update-sources
+        sudo systemctl restart suricata
+
+        echo "¡Suricata reiniciado!"
+    else
+        echo "No se hicieron cambios. Saliendo..."
+    fi
+
 fi
